bf9b762a7dcca28b06700270ee7829d9d4225116
Lenoxlkz <198505191+Lenoxlkz@users.noreply.github.com>
2026-09-01 00:27:09 -0600

    Delete test-yt.js

--- test-yt.js
+++
@@ -1,4 +0,0 @@
-const { extractYoutubeMedia } = require('./lib/mediaExtractor.ts');
-extractYoutubeMedia('https://www.youtube.com/watch?v=dQw4w9WgXcQ').then(res => {
-  console.log('Result:', res);
-}).catch(console.error);
